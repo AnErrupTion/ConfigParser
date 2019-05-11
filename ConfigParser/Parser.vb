09bb3cb25039e0e97db146a374556fd4790f0078
AnErrupTion <49339966+AnErrupTion@users.noreply.github.com>
2019-05-11 16:15:36 +0200

    Add files via upload

---
+++ ConfigParser/Parser.vb
@@ -0,0 +1,26 @@
+﻿Imports System.IO
+
+Public Class Parser
+
+    Public ConfigFile As String
+
+    Public Sub New(ByVal ConfigFile As String)
+        Me.ConfigFile = ConfigFile
+    End Sub
+
+    Public Function ReadConfig(ByVal Param As String)
+        Dim TheLine As String
+
+        Using Reader As New StreamReader(ConfigFile)
+            While Not Reader.EndOfStream
+                Dim Line As String = Reader.ReadLine()
+                If Line.Contains(Param) Then
+                    TheLine = Line
+                End If
+            End While
+        End Using
+
+        Return TheLine
+    End Function
+
+End Class
